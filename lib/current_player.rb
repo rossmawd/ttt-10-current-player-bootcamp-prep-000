d7a5e63efe9336acd2956df95fd00c520221fbac
global-debugger-9436 <rossmawd@gmail.com>
2019-03-10 12:34:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@ turns = 0
   turns
 end
 
-puts "the number of turns is #{turn_count(["X","O","O","","O","X","X","X","X"])}"
+
 
 def current_player(turn_count)
   if turn_count % 2 == 0 
@@ -17,3 +17,5 @@ def current_player(turn_count)
   else
     return "O"
   end
+  
+  puts "the number of turns is #{turn_count(["X","O","O","","O","X","X","X","X"])}"
